0ad780e02f314dc17ada1c196045ec8d1bbec152
ayaan03-android <76834976+ayaan03-android@users.noreply.github.com>
2021-05-18 19:25:54 +0530

    First Commit

--- PixelWallpaper/app/src/main/java/com/aatec/pixelwallpaper/MainActivity.kt
+++ PixelWallpaper/app/src/main/java/com/aatec/pixelwallpaper/MainActivity.kt
@@ -3,6 +3,12 @@ package com.aatec.pixelwallpaper
 import android.os.Bundle
 import android.viewbinding.library.activity.viewBinding
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.aatec.pixelwallpaper.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,10 +16,37 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     private val binding: ActivityMainBinding by viewBinding()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragmentImage
+            )
+        )
+        binding.apply {
+            setSupportActionBar(mainLayout.toolbar)
+            setupActionBarWithNavController(navController)
+            bottomNavigation.apply {
+                setOnNavigationItemReselectedListener { }
+                bottomNavigation.setupWithNavController(navController)
+            }
+        }
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
